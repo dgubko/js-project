3ff590a87c45656424b7fd44d306c3a0da99ed70
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-08-03 10:49:11 -0600

    Add comments

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 var name = ["John", "Peter", "Alex", "Bill", "Sam"];
 var age = [25, 36, 18, 40, 32];
 var answers = [true, false, false, true, false];
+name.pop(); // Will delete the last subject
+console.log(name); 
+age.shift(); // Will delete the first subject
+console.log(age);
+answers.push(true); // Will add subject in the end
+console.log(answers);
