ebd27f3056f1958e43d5d00322a8fb3b676bf43b
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-08-03 11:49:43 -0600

    Add explanation

--- arrays.js
+++ arrays.js
@@ -7,3 +7,5 @@ age.shift(); // Will delete the first subject
 console.log(age);
 answers.push(true); // Will add subject in the end
 console.log(answers);
+
+console.log(age[1]); // (Explanation) Will print the second character of an age Array
